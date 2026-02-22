387285801f6eddca06bda408210f1ae43362a3a7
Cal Corbin <calcorbin@gmail.com>
2026-02-22 12:17:16 -0600

    Add components to page.tsx

---
+++ src/app/components/features/MagicCard/MagicCard.tsx
@@ -0,0 +1,3 @@
+export const MagicCard = ({ title }: { title: string }) => {
+  return <div>{title}</div>;
+};
---
+++ src/app/components/ui/NetworkGraph/NetworkGraph.tsx
@@ -0,0 +1,10 @@
+import { MagicCard } from '@/app/components/features/MagicCard/MagicCard';
+
+export const NetworkGraph = () => {
+  return (
+    <div>
+      NetworkGraph
+      <MagicCard title="Aust Com" />
+    </div>
+  );
+};
